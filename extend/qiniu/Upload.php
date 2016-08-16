eaa934aa7faf43a88ccc76142c3ab11696da1c1b
zhgxun <zhgxun1989@163.com>
2016-08-16 22:41:47 +0800

    add qiniu extend

---
+++ extend/qiniu/Upload.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace common\extend\qiniu;
+
+require __DIR__ . '/autoload.php';
+
+use Qiniu\Auth;
+use Qiniu\Storage\BucketManager;
+use Qiniu\Storage\UploadManager;
+
+class Upload
+{
+    private $accessKey;
+    private $secretKey;
+    private $bucket;
+    private $auth;
+    private $token;
+    public static $instance;
+
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        $this->accessKey = \Yii::$app->params['qiniu']['ak'];
+        $this->secretKey = \Yii::$app->params['qiniu']['sk'];
+        $this->auth = new Auth($this->accessKey, $this->secretKey);
+        $this->bucket = \Yii::$app->params['qiniu']['bucket'];
+        $this->token = $this->auth->uploadToken($this->bucket);
+    }
+
+    /**
+     * 上传一段文本内容
+     * @param string $content 需要上传的文本内容
+     * @return mixed ['hash' => '', 'key' => ''] 成功后的文件名保存在key=>value中
+     */
+    public function uploadText($content)
+    {
+        list($ret, $error) = (new UploadManager())->put($this->token, null, $content);
+        if ($error !== null) {
+            return $error;
+        } else {
+            return $ret;
+        }
+    }
+
+    /**
+     * 上传图片
+     * @param string $fileName 图片名
+     * @param string $filePath 图片存放路径
+     * @return mixed ['hash' => '', 'key' => ''] 成功后的文件名保存在key=>value中
+     * @throws \Exception
+     */
+    public function uploadFile($fileName, $filePath = './')
+    {
+        list($ret, $error) = (new UploadManager())->putFile($this->token, $fileName, $filePath);
+        if ($error !== null) {
+            return $error;
+        } else {
+            return $ret;
+        }
+    }
+
+    /**
+     * 删除文件
+     * @param string $fileName 文件名
+     * @return mixed
+     */
+    public function delete($fileName)
+    {
+        return (new BucketManager($this->auth))->delete($this->bucket, $fileName);
+    }
+}
